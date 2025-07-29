558c31949d0442a427d10aaaeb79f885cd219d21
georgy <gessegeorgy@gmail.com>
2025-07-29 17:27:12 -0400

    test

--- notes.nvim.sh
+++ notes.nvim.sh
@@ -3,16 +3,19 @@
 # Welcome to notes.nvim enjoy your stay
 
 # set what directory to use for notes
-notedir=/home/georgy/Documents/notes.nvim.sh
 # set what file to use as template
-template=/home/georgy/Documents/notes.nvim/template.md
+for arg in "$@"; do
+  eval "$arg"
+done
+echo "Notes directory is: ${notes_dir}"
+echo "Template in use is: ${template_path}"
 
 # create the date dir string
+full_date=$(date +%F)
 year=$(date +%Y)
 month=$(date +%m)
 day=$(date +%d)
-date_dir_str="${notedir}/${year}/${month}/${day}"
-echo $date_dir_str
+date_dir_str="${notes_dir}/${year}/${month}/${day}"
 
 # create the directory if it doesn't exist
 mkdir -p $date_dir_str
@@ -26,9 +29,9 @@ if [ -f "$daily_path_str" ]; then
 else
     echo "Daily file does not exist."
     # create the file using a template
-    cp $template $daily_path_str
+    cp $template_path $daily_path_str
     # here is the template :)
-    echo $(date %F) >> $daily_path_str
+    echo \# $full_date >> $daily_path_str
 fi
 
 # now open nvim on that file
