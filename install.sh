acf8c6c1c8350f71fa9a287991ab3bbaf03c9793
Path-17 <83365777+Path-17@users.noreply.github.com>
2025-07-29 01:44:17 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,6 @@ rm -rf .git.
 chmod +x notes.nvim.sh
 read -p "Enter the path to your .bashrc (or equivalent) file [default: ~/.bashrc]: " bashrc 
 bashrc_path=${bashrc:-~/.bashrc}
-echo $bashrc_path
 read -p "Enter the note directory you want to use: [default: ./]: " notes
 notes_dir=${notes:-$(pwd)}
 read -p "Enter the path to the daily template: [default: ./notes.nvim/template.md]: " template
